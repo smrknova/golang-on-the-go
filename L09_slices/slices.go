41949dce865dc1601fb19ad343802976bf1ef190
smrk2415 <smarakgartia2415@gmail.com>
2025-06-14 20:15:50 +0530

    Refactor slice initialization and manipulation examples in slices.go

--- L09_slices/slices.go
+++ L09_slices/slices.go
@@ -12,16 +12,58 @@ func main() {
 	// fmt.Println(nums == nil)
 	// fmt.Println(len(nums))
 
-	var nums = make([]int, 2, 5)
+	// var nums = make([]int, 2, 5)
 	// capacity -> maximum numbers of elements can fit
-	fmt.Println(cap(nums))
-	fmt.Println(nums)
+	// fmt.Println(cap(nums))
+	// fmt.Println(nums)
+
+	// nums = append(nums, 1)
+	// nums = append(nums, 2)
+	// nums = append(nums, 3)
+	// nums = append(nums, 4)
+
+	// fmt.Println(nums)
+	// fmt.Println(cap(nums))
+
+	// var nums2 = make([]int, 0, 5)
+	// nums2 = append(nums2, 1)
+	// nums2 = append(nums2, 2)
+	// fmt.Println(nums2)
+
+
+	nums := []int{}
 
 	nums = append(nums, 1)
 	nums = append(nums, 2)
-	nums = append(nums, 3)
-	nums = append(nums, 4)
 
+	nums[0] = 3
+	nums[1] = 5
 	fmt.Println(nums)
 	fmt.Println(cap(nums))
+	fmt.Println(len(nums))
+
+	// var nums = make([]int, 0, 5)
+	// nums = append(nums, 2)
+	// var nums2 = make([]int, len(nums))
+
+	// // copy function
+	// copy(nums2, nums)
+	// fmt.Println(nums, nums2)
+
+	// slice operator
+
+	// var nums = []int{1, 2, 3, 4, 5}
+	// fmt.Println(nums[0:1])
+	// fmt.Println(nums[:2])
+	// fmt.Println(nums[1:])
+
+	// slices
+	// var nums1 = []int{1, 2, 3}
+	// var nums2 = []int{1, 2, 4}
+
+	// fmt.Println(slices.Equal(nums1, nums2))
+
+	// var nums = [][]int{{1, 2, 3}, {4, 5, 6}}
+	// fmt.Println(nums)
+
 }
